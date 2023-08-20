0d09a1014e215cd498cff30fd9611cffb71375d2
varshi2003 <113238130+varshi2003@users.noreply.github.com>
2023-08-20 18:12:05 +0530

    Create longest_common_prefix.cpp

---
+++ longest_common_prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string ans;
+        for(int i=0;i<strs[0].length();i++)
+        {
+            if(strs[0][i]!=strs[strs.size()-1][i]) break;
+            else 
+            ans+=strs[0][i];
+        }
+        return ans;
+    }
+};
